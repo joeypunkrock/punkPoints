5bff785c8a94c0712e859b2a2f98e425f7ac095c
Joey Thomas <joey@joeythomas93.com>
2016-01-31 16:52:51 +0000

    Window now scrolls to title on load. Removed nottify message for index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include 'inc/config.php';
 </head>
 <img class="gg" src="img/gg.png">
 <hr style="margin-top:-10px;">
-<h1>Punk Points</h1>
+<h1 id="top">Punk Points</h1>
 <hr>
 
 <?php
@@ -62,26 +62,7 @@ $mysqli->close();
 </html>
 
 <script type='text/javascript'>
-  // // Let's check if the browser supports notifications
-  // if (!("Notification" in window)) {
-  //   alert("This browser does not support desktop notification");
-  // }
-
-  // // Let's check whether notification permissions have already been granted
-  // else if (Notification.permission === "granted") {
-  //   // If it's okay let's create a notification
-  //   var notification = new Notification("Points updated since last check");
-  // }
-
-  // // Otherwise, we need to ask the user for permission
-  // else if (Notification.permission !== 'denied') {
-  //   Notification.requestPermission(function (permission) {
-  //     // If the user accepts, let's create a notification
-  //     if (permission === "granted") {
-  //       var notification = new Notification("Points updated since last check");
-  //     }
-  //   });
-  // }
+	window.onload = window.location.hash = 'top';
 </script>"
 
 
