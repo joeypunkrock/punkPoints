58abd64106eb1654740a0f666d577c21837e0446
Joey Thomas <joey@joeythomas93.com>
2016-01-31 14:39:14 +0000

    Temp removed notify system

--- index.php
+++ index.php
@@ -62,26 +62,26 @@ $mysqli->close();
 </html>
 
 <script type='text/javascript'>
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
+  // // Let's check if the browser supports notifications
+  // if (!("Notification" in window)) {
+  //   alert("This browser does not support desktop notification");
+  // }
 
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification("Points updated since last check");
-  }
+  // // Let's check whether notification permissions have already been granted
+  // else if (Notification.permission === "granted") {
+  //   // If it's okay let's create a notification
+  //   var notification = new Notification("Points updated since last check");
+  // }
 
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification("Points updated since last check");
-      }
-    });
-  }
+  // // Otherwise, we need to ask the user for permission
+  // else if (Notification.permission !== 'denied') {
+  //   Notification.requestPermission(function (permission) {
+  //     // If the user accepts, let's create a notification
+  //     if (permission === "granted") {
+  //       var notification = new Notification("Points updated since last check");
+  //     }
+  //   });
+  // }
 </script>"
 
 
